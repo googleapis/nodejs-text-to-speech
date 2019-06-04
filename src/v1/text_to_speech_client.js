740924eff44d4a8fab60563e955f8fa88212522d
yoshi-automation <yoshi-automation@google.com>
2019-06-04 12:45:23 -0700

    [CHANGE ME] Re-generated  to pick up changes in the API or client library generator.

--- src/v1/text_to_speech_client.js
+++ src/v1/text_to_speech_client.js
@@ -57,12 +57,15 @@ class TextToSpeechClient {
   constructor(opts) {
     this._descriptors = {};
 
+    const servicePath =
+      opts.servicePath || opts.apiEndpoint || this.constructor.servicePath;
+
     // Ensure that options include the service address and port.
     opts = Object.assign(
       {
         clientConfig: {},
         port: this.constructor.port,
-        servicePath: this.constructor.servicePath,
+        servicePath,
       },
       opts
     );
@@ -138,6 +141,14 @@ class TextToSpeechClient {
    * The DNS address for this API service.
    */
   static get servicePath() {
+    return 'texttospeech.googleapis.com';
+  }
+
+  /**
+   * The DNS address for this API service - same as servicePath(),
+   * exists for compatibility reasons.
+   */
+  static get apiEndpoint() {
     return 'texttospeech.googleapis.com';
   }
 
