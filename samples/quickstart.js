9a83409a0d04451436f83796489224dfaec7e38d
Leah Cole <coleleah@google.com>
2019-12-30 15:04:16 -0800

    fix: revert weird lowercases

--- samples/quickstart.js
+++ samples/quickstart.js
@@ -16,38 +16,38 @@
 
 function main(
 ){
-  // [start tts_quickstart]
-  // imports the google cloud client library
+  // [START tts_quickstart]
+  // Imports the Google Cloud client library
   const textToSpeech = require('@google-cloud/text-to-speech');
 
-  // import other required libraries
+  // Import other required libraries
   const fs = require('fs');
   const util = require('util');
-  // creates a client
+  // Creates a client
   const client = new textToSpeech.TextToSpeechClient();
   async function quickStart() {
 
-    // the text to synthesize
+    // The text to synthesize
     const text = 'hello, world!';
 
-    // construct the request
+    // Construct the request
     const request = {
       input: {text: text},
-      // select the language and ssml voice gender (optional)
-      voice: {languageCode: 'en-us', ssmlGender: 'neutral'},
+      // Select the language and SSML voice gender (optional)
+      voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
       // select the type of audio encoding
       audioConfig: {audioEncoding: 'MP3'},
     };
 
-    // performs the text-to-speech request
+    // Performs the text-to-speech request
     const [response] = await client.synthesizeSpeech(request);
-    // write the binary audio content to a local file
+    // Write the binary audio content to a local file
     const writeFile = util.promisify(fs.writeFile);
     await writeFile('output.mp3', response.audioContent, 'binary');
     console.log('audio content written to file: output.mp3');
   }
   quickStart();
-  // [end tts_quickstart]
+  // [END tts_quickstart]
 }
 
 main(...process.argv.slice(2));
